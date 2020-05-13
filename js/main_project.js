d5e0e9abc385e204b4f8d79628f2f1bf52c5c991
EmmaMaddock1 <43006342+EmmaMaddock1@users.noreply.github.com>
2020-05-13 11:06:27 -0500

    Add files via upload

---
+++ js/main_project.js
@@ -0,0 +1,27 @@
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
+
+$(document).ready(function() {
+
+	var rellax = new Rellax('.rellax');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
